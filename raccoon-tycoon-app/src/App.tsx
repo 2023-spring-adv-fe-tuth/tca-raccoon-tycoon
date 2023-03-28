1c7fe39943327438bc9c329c90307b7a27539f08
stephfrankable <112651864+stephfrankable@users.noreply.github.com>
2023-03-28 15:24:50 -0500

    Added Home component
    With Bootstrap styled placeholders, specifically Button and Card.

--- raccoon-tycoon-app/src/App.tsx
+++ raccoon-tycoon-app/src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Home = () => (
+  <>
+    <h1>
+      TCA Raccoon Tycoon
+    </h1>
+      <h2>
+        Companion App
+      </h2>
+      <Button variant="primary">
+        Select Players
+      </Button>{' '}
+      <Card>
+        <Card.Header>
+          Leaderboard
+        </Card.Header>
+        <Card.Body>
+          Play a game to begin your leaderboard.
+        </Card.Body>
+      </Card>
+
+  </>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
