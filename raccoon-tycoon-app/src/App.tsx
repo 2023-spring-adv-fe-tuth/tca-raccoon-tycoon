ee4d36ad073662d4b6512cc962cf710df15893a1
stephfrankable <112651864+stephfrankable@users.noreply.github.com>
2023-04-03 15:16:38 -0500

    screens/initial-navigation
    Barebones working - need to add buttons

--- raccoon-tycoon-app/src/App.tsx
+++ raccoon-tycoon-app/src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Home } from './Home';
+import { Setup } from './Setup';
+import { Finish } from './Finish';
 
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
@@ -17,7 +20,13 @@ function App() {
         Companion App
       </h2>
       <hr />
-      <Home />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<Setup />} />
+          <Route path="/finish" element={<Finish />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
